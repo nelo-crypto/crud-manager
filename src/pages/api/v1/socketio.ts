e475418b61317c57e4c0b37dcbb3712e63766f57
Nelo Crypto <nelo.crypto@protonmail.com>
2022-06-14 17:15:08 +0100

    Await approach

--- src/pages/api/v1/socketio.ts
+++ src/pages/api/v1/socketio.ts
@@ -8,55 +8,52 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse,
 ) {
-    prisma.option
+    const option = await prisma.option
         .findUnique({
             where: {
                 name: 'Spread',
             }
         })
-        .then(async (option: Option) => {
-            const kraken = new Kraken()
-            const io = new Server(res.socket.server)
 
-            let priceSpread: number = 0
+    if (option === null) {
+        res.end()
 
-            if (res.socket.server.io) {
-                console.debug('Socket is already running')
-            } else {
-                console.debug('Socket is initializing')
+        return
+    }
 
-                res.socket.server.io = io
+    const kraken = new Kraken()
+    const io = new Server(res.socket.server)
 
-                io.on('connect', socket => {
-                    console.debug('Socket connected!')
-                })
-            }
+    let priceSpread: number = 0
 
-            setInterval(() => {
-                if (!io) return
+    if (res.socket.server.io) {
+        console.debug('Socket is already running')
+    } else {
+        console.debug('Socket is initializing')
 
-                io.sockets.emit('price-change', priceSpread)
-            }, 10000)
+        res.socket.server.io = io
 
-            const book100 = await kraken.ws.book({depth: 10})
-                // live book construction from "snapshot", "ask", and "bid" events.
-                .on('mirror', (mirror, pair) => {
-                    const betterAsk: number = parseFloat(mirror.as[0][0])
-                    const betterBid: number = parseFloat(mirror.bs[0][0])
-                    const price: number = (betterAsk + betterBid) / 2
-
-                    priceSpread = (1 + option.value / 100) * price
-                })
-                .subscribe('XBT/USD') // subscribe to multiple pairs at once
-
-            res.end()
+        io.on('connect', socket => {
+            console.debug('Socket connected!')
         })
-        .catch((e) => {
-            console.error(ERROR.E0004.code, ERROR.E0004.reason, e)
+    }
 
-            res.status(500).json({
-                error_code: ERROR.E0004.code,
-                message: 'Please contact support',
-            })
+    setInterval(() => {
+        if (!io) return
+
+        io.sockets.emit('price-change', priceSpread)
+    }, 10000)
+
+    const book100 = await kraken.ws.book({depth: 10})
+        // live book construction from "snapshot", "ask", and "bid" events.
+        .on('mirror', (mirror, pair) => {
+            const betterAsk: number = parseFloat(mirror.as[0][0])
+            const betterBid: number = parseFloat(mirror.bs[0][0])
+            const price: number = (betterAsk + betterBid) / 2
+
+            priceSpread = (1 + option.value / 100) * price
         })
+        .subscribe('XBT/USD') // subscribe to multiple pairs at once
+
+    res.end()
 }
