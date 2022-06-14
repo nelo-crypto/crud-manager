3c28970d3c13faf0baff4f96303cac8f490b24db
Nelo Crypto <nelo.crypto@protonmail.com>
2022-06-14 13:44:56 +0100

    Add healthcheck.test.ts working

--- src/pages/api/v1/healthcheck.test.ts
+++ src/pages/api/v1/healthcheck.test.ts
@@ -1,35 +1,18 @@
 import handler from './healthcheck'
-import {createMocks, RequestMethod} from 'node-mocks-http'
-import type {NextApiRequest, NextApiResponse} from 'next'
+import {createMocks} from 'node-mocks-http'
 
-describe('Healthcheck', () => {
-    function mockRequestResponse(method: RequestMethod = 'GET') {
-        const {
-            req,
-            res,
-        }: { req: NextApiRequest; res: NextApiResponse } = createMocks({method})
+describe('/api/healthcheck', () => {
+    test('should return 200 and a string', async () => {
+        const { req, res } = createMocks({
+            method: 'GET'
+        });
 
-        req.headers = {
-            'Content-Type': 'application/json',
-            //'X-SESSION-TOKEN': authToken,
-        }
-        //req.query = {gatewayID: `${gatewayID}`}
-
-        return {req, res}
-    }
-
-    test('that the application is live with a status of 200', () => {
-        const { req, res } = mockRequestResponse();
-        const resMock = {status: jest.fn()} // Mocks `res`
-        const resStatusMock = {json: jest.fn()} // Mock `res.status`
-
-        resMock.status.mockReturnValue(resStatusMock) // Makes `res.status` return `resStatusMock`
-
-        handler(req, resMock)
-
-        expect(resMock.status).toHaveBeenCalledWith(200)
-        expect(resStatusMock.json).toHaveBeenCalledWith({
-            message: 'Hello from Next.js!'
-        })
-    })
-})
+        await handler(req, res);
+        expect(res._getStatusCode()).toBe(
+            200
+        );
+        expect(
+            JSON.parse(res._getData())
+        ).toHaveProperty('message');
+    });
+});
